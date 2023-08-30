9514fda2b71375f4a4018eb20c512e75b5dd63bd
Parag <paragpurandare50@gmail.com>
2023-08-30 17:29:23 +0530

    Done with api integration with conversation form conversation/api added with axios and openai for conversation

--- app/(dashboard)/(routes)/image/page.tsx
+++ app/(dashboard)/(routes)/image/page.tsx
@@ -1,12 +1,111 @@
-import { Heading, Image, ImageIcon, MessageSquare } from 'lucide-react'
-import React from 'react'
+"use client";
+import axios from "axios";
+import * as z from "zod";
+import Heading from "@/components/heading";
+import { ImageIcon, MessageSquareIcon } from "lucide-react";
+import React, { useState } from "react";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { formSchema } from "./constants";
+import { Form, FormControl, FormField, FormItem } from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+import { useRouter } from "next/navigation";
+import { ChatCompletionMessage } from "openai/resources/chat/index.mjs";
+import { Empty } from "@/components/empty";
+import Loader from "@/components/Loader";
 
 const ImageGeneration = () => {
+  const router = useRouter();
+  const [messages, setMessages] = useState<any[]>([]);
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      prompt: "",
+    },
+  });
+
+  const isLoading = form.formState.isSubmitting;
+  const onSumbit = async (values: z.infer<typeof formSchema>) => {
+    try {
+      const userMessage: any = {
+        role: "user",
+        content: values.prompt,
+      };
+      const newMessages = [...messages, userMessage];
+      const response = await axios.post("/api/conversation", {
+        messages: newMessages,
+      });
+      setMessages((current) => [...current, userMessage, response.data]);
+      form.reset();
+    } catch (error: any) {
+      //open pro model subscription
+      console.log(error);
+    } finally {
+      router.refresh();
+    }
+  };
+
   return (
     <div>
-      hello there convert your prompt to image
+      <Heading
+        title="Image Generation"
+        description="Convert your prompt into image"
+        icon={ImageIcon}
+        iconColor="text-violet-700"
+        bgColor="bg-violet-700/10"
+      />
+      <div className="px-4 lg:px-8">
+        <div>
+          <Form {...form}>
+            <form
+              onSubmit={form.handleSubmit(onSumbit)}
+              className="rounded-lg border w-full p-4 px-3 md:px-6 focus-within:shadow-sm grid grid-cols-12 gap-2"
+            >
+              <FormField
+                name="prompt"
+                render={({ field }) => (
+                  <FormItem className="col-span-12 lg:col-span-10">
+                    <FormControl className="m-0 p-0">
+                      <Input
+                        className="border-0 outline-none focus-visible:ring-0 focus-visible:ring-transparent"
+                        disabled={isLoading}
+                        placeholder="A blue whale Jumping up form an ocean"
+                        {...field}
+                      />
+                    </FormControl>
+                  </FormItem>
+                )}
+              />
+              <Button
+                className="col-span-12 lg:col-span-2 w-full"
+                disabled={isLoading}
+              >
+                Generate
+              </Button>
+            </form>
+          </Form>
+        </div>
+        <div className="space-y-4 mt-4 ">
+          {isLoading && (
+            <div className="p-8 rounded-lg w-full flex items-center justify-center bg-muted">
+              <Loader />
+            </div>
+          )}
+          {messages.length === 0 && !isLoading && (
+              <Empty label={"Give a prompt to generate image."} />
+          )}
+          <div className="flex flex-col-reverse gap-y-4">
+            {messages.map((message) => (
+              <div key={message.content}>
+                {message.content}
+              </div>
+            ))}
+          </div>
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default ImageGeneration
+export default ImageGeneration;
