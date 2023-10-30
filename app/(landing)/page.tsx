d0a3272956941627f865a0961fcd2f08f001420e
Parag <paragpurandare50@gmail.com>
2023-10-31 00:50:17 +0530

    Started the landing page created basic structure

--- app/(landing)/page.tsx
+++ app/(landing)/page.tsx
@@ -1,19 +1,22 @@
 'use client'
-import { Button } from "@/components/ui/button";
-import {LogIn } from "lucide-react";
+
+import { LandingNavbar } from "@/components/LandingNavbar";
 
 const LandingPage = () => {
   return (
-    <div>
-      <div>Landing Page (Unprotected)</div>
-      <LogIn></LogIn>
-      <a href="/sign-in">
-        <Button>Login</Button>
-      </a>
-      <a href="/sign-up">
-        <Button>Register</Button>
-      </a>
+    <div className="h-full">
+      <LandingNavbar />
     </div>
+    // <div>
+    //   <div>Landing Page (Unprotected)</div>
+    //   <LogIn></LogIn>
+    //   <a href="/sign-in">
+    //     <Button>Login</Button>
+    //   </a>
+    //   <a href="/sign-up">
+    //     <Button>Register</Button>
+    //   </a>
+    // </div>
   );
 };
 export default LandingPage;
